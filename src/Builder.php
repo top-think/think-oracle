5d8d6d0cde592a7f2054f6c6ce3367c7354610d0
thinkphp <thinkphp@qq.com>
2018-01-29 14:06:55 +0800

    改进

--- src/Builder.php
+++ src/Builder.php
@@ -60,12 +60,12 @@ class Builder extends BaseBuilder
 
     /**
      * 字段和表名处理
-     * @access protected
+     * @access public
      * @param  Query      $query        查询对象
      * @param  string     $key
      * @return string
      */
-    protected function parseKey(Query $query, $key)
+    public function parseKey(Query $query, $key)
     {
         $key = trim($key);
 
